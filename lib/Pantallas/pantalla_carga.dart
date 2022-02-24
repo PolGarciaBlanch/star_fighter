c1895e6f89a519b0fbdf726d0502c8ab19534c2b
DavidRaulMiranda <78372708+DavidRaulMiranda@users.noreply.github.com>
2022-02-24 18:28:50 +0100

    modificado pantalla de carga
    imagenes funcionan correctamente

--- lib/Pantallas/pantalla_carga.dart
+++ lib/Pantallas/pantalla_carga.dart
@@ -20,46 +20,60 @@ class _PantallaCargaState extends State<PantallaCarga> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Image.asset('assets/img/backGround.jpg'),
-        Container(
-          padding: EdgeInsets.all(20),
-          width: 500,
-          height: 800,
-          color: Colors.red,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image(image: pizzaAsset, width: 400, height: 400),
-              Image.asset('assets/img/backGround.jpg'),
-              Image.asset('assets/img/backGround.jpg'),
-              Container(
-                width: 80,
-                height: 20,
-                child: Text(
-                  appTip,
-                  textDirection: TextDirection.ltr,
-                  style: const TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                  ),
+        body: Container(
+            width: 600,
+            height: 800,
+            color: Colors.black,
+            child: (Stack(
+              children: [
+                Image.asset(
+                  'assets/img/backGround.jpg',
+                  width: 500,
+                  height: 1000,
                 ),
-                decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.9),
-                    border: Border.all(color: Colors.black),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black,
-                        offset: Offset(2.0, 2.0),
+                Container(
+                  padding: const EdgeInsets.only(
+                      left: 20, top: 50, right: 20, bottom: 20),
+                  width: 800,
+                  height: 800,
+                  color: Colors.red.withOpacity(0.02),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Image.asset('assets/img/AppLogo.png'),
+                      Container(
+                        width: 300,
+                        height: 300,
+                        child: Image.asset('assets/img/xWing.png'),
+                      ),
+                      Container(
+                        width: 500,
+                        height: 100,
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              appTip,
+                              textDirection: TextDirection.ltr,
+                              style: const TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.yellow),
+                            )),
+                        decoration: BoxDecoration(
+                            color: Colors.redAccent,
+                            border: Border.all(color: Colors.yellow),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.black,
+                                offset: Offset(2.0, 2.0),
+                              )
+                            ]),
                       )
-                    ]),
-              )
-            ],
-          ),
-        )
-      ],
-    ));
+                    ],
+                  ),
+                )
+              ],
+            ))));
   }
 }
